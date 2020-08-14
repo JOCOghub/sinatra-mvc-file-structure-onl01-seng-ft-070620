1c376e329891746462a1c200b82953dc0ca45fcb
Joseph cohen <jojocohen57@gmail.com>
2020-08-14 21:23:39 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,8 +1,13 @@
 class Dog
   attr_accessor :name, :breed, :age
+  @@all = [] 
   def initialize(name, breed, age)
     @name = name  
     @breed = breed
     @age =age
+  end   
+  
+  def self.all 
+    @@all
   end   
 end 
